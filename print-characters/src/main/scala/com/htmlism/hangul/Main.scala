86cf0bac1c4901bddf24d41a3864b4438eae9c83
Mark Canlas <github@htmlism.com>
2015-08-26 01:00:17 -0400

    made initialization predictable

--- print-characters/src/main/scala/com/htmlism/hangul/Main.scala
+++ print-characters/src/main/scala/com/htmlism/hangul/Main.scala
@@ -1,6 +1,6 @@
 package com.htmlism.hangul
 
-object Main extends App {
+object Hangul {
   val initialOriginCodePoint = 0x1100
   val medialOriginCodePoint  = 0x1161
   val finalOriginCodePoint   = 0x11A8
@@ -8,6 +8,10 @@ object Main extends App {
   val initialConsonants = 19
   val vowels = 21
   val finalConsonants = 27
+}
+
+object Main extends App {
+  import com.htmlism.hangul.Hangul._
 
   // optional final constant makes 28
   // 19 * 21 * 28 = 11,172
