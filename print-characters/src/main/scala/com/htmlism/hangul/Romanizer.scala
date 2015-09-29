0303a67ec1d97e62d00214eb111700ab8972834a
Mark Canlas <github@htmlism.com>
2015-09-29 18:49:44 -0400

    added static romanizer

--- print-characters/src/main/scala/com/htmlism/hangul/Romanizer.scala
+++ print-characters/src/main/scala/com/htmlism/hangul/Romanizer.scala
@@ -1,5 +1,30 @@
 package com.htmlism.hangul
 
+object Romanizer {
+  def applyWhen(str: String, filter: Char => Boolean, romanize: String => String): String = {
+    val res = new StringBuilder
+    var acc = new StringBuilder
+
+    for (c <- str)
+      if (filter(c))
+        acc += c
+      else {
+        val hangul = acc.result()
+
+        if (hangul.nonEmpty) {
+          res ++= romanize(hangul)
+
+          // reset the accumulator
+          acc = new StringBuilder
+        }
+
+        res += c
+      }
+
+    res.result()
+  }
+}
+
 trait Romanizer {
   def romanize(hangul: String): String
 }
