2d5e29f6f5354b5df089f9649306f58949018bde
Mark Canlas <github@htmlism.com>
2026-09-01 20:32:39 -0400

    replace ??? with empty string stubs in romanizers :owl:

--- print-characters/src/main/scala/com/htmlism/hangul/MakuRomanizer.scala
+++ print-characters/src/main/scala/com/htmlism/hangul/MakuRomanizer.scala
@@ -4,5 +4,5 @@ package com.htmlism.hangul
   * My own stab at romanization that isn't the common one
   */
 class MakuRomanizer extends Romanizer {
-  def romanize(hangul: String): String = ???
+  def romanize(hangul: String): String = ""
 }
